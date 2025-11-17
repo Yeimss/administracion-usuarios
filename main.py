a54eb241358b610c018cff137a4eb8ee103b5493
Sebastian <sebastian@MacBook-Pro-de-Sebastian.local>
2025-11-17 12:14:00 -0500

    Creación de clase

---
+++ main.py
@@ -0,0 +1,8 @@
+from models.Usuario import Usuario
+
+def main():
+    objsebas = Usuario()
+    print(objsebas.profesion)
+
+if __name__ == "__main__": 
+    main()
---
+++ models/Usuario.py
@@ -0,0 +1,7 @@
+class Usuario:
+    def __init__(self):
+        self.nombre = "Sebas"
+        self.apellido = "Alzate"
+        self.documento = "1035880432"
+        self.ciudad = "Girardota"
+        self.profesion = "Ingeniero"
